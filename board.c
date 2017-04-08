56cf93b524e8a364270654f28f87af678e64c190
Clay Dowling <clay@lazarusid.com>
2017-04-08 07:33:24 -0400

    Updated formatting

--- board.c
+++ board.c
@@ -1,31 +1,23 @@
+#include "board.h"
 #include <assert.h>
 #include <string.h>
-#include "board.h"
 
-bool		board     [X_MAX][Y_MAX];
+bool board[X_MAX][Y_MAX];
 
-void 
-board_init()
-{
-	memset(board, 0, sizeof(board));
+void board_init() { memset(board, 0, sizeof(board)); }
+
+bool board_at(unsigned int x, unsigned int y) {
+  assert(x >= 0);
+
+  if (x >= X_MAX) {
+    x = x % X_MAX;
+  }
+  if (y >= Y_MAX) {
+    return false;
+  }
+  return board[x][y];
 }
 
-bool 
-board_at(unsigned int x, unsigned int y)
-{
-	assert(x >= 0);
-
-	if (x >= X_MAX) {
-		x = x % X_MAX;
-	}
-	if (y >= Y_MAX) {
-		return false;
-	}
-	return board[x][y];
-}
-
-void 
-board_set(unsigned int x, unsigned int y, bool value)
-{
-    board[x][y] = value;
+void board_set(unsigned int x, unsigned int y, bool value) {
+  board[x][y] = value;
 }
--- test/test_board.c
+++ test/test_board.c
@@ -1,95 +1,86 @@
-#include <check.h>
 #include "../board.h"
+#include <check.h>
 
-START_TEST(board_init_setsAllFieldsFalse)
-{
-	int		x;
-	int		y;
-	bool		foundAlive = false;
+START_TEST(board_init_setsAllFieldsFalse) {
+  int x;
+  int y;
+  bool foundAlive = false;
 
-	board[0][0] = true;
-	board_init();
+  board[0][0] = true;
+  board_init();
 
-	for (x = 0; x < X_MAX; ++x) {
-		for (y = 0; y < Y_MAX; ++y) {
-			if (board[x][y]) {
-				foundAlive = true;
-			}
-		}
-	}
+  for (x = 0; x < X_MAX; ++x) {
+    for (y = 0; y < Y_MAX; ++y) {
+      if (board[x][y]) {
+        foundAlive = true;
+      }
+    }
+  }
 
-	ck_assert_int_eq(foundAlive, false);
+  ck_assert_int_eq(foundAlive, false);
 }
 END_TEST
 
-START_TEST(boardAt_withLocationTrue_returnsTrue)
-{
-	board_init();
-	board[1][1] = true;
+START_TEST(boardAt_withLocationTrue_returnsTrue) {
+  board_init();
+  board[1][1] = true;
 
-	ck_assert_int_eq(true, board_at(1, 1));
+  ck_assert_int_eq(true, board_at(1, 1));
 }
 END_TEST
 
-START_TEST(boardAt_withLocationFalse_returnsFalse)
-{
-	board_init();
-	board[1][1] = false;
+START_TEST(boardAt_withLocationFalse_returnsFalse) {
+  board_init();
+  board[1][1] = false;
 
-	ck_assert_int_eq(false, board_at(1, 1));
+  ck_assert_int_eq(false, board_at(1, 1));
 }
 END_TEST
 
-START_TEST(boardAt_wrapsOnXAxis)
-{
-	board_init();
-	board[0][0] = true;
-	ck_assert_int_eq(true, board_at(X_MAX, 0));
+START_TEST(boardAt_wrapsOnXAxis) {
+  board_init();
+  board[0][0] = true;
+  ck_assert_int_eq(true, board_at(X_MAX, 0));
 }
 END_TEST
 
-START_TEST(boardAt_withYOverMax_returnsFalse)
-{
-	board_init();
-	for (int x = 0; x < X_MAX; ++x) {
-		for (int y = 0; y < Y_MAX; ++y) {
-			board[x][y] = true;
-		}
-	}
-	ck_assert_int_eq(false, board_at(0, Y_MAX));
+START_TEST(boardAt_withYOverMax_returnsFalse) {
+  board_init();
+  for (int x = 0; x < X_MAX; ++x) {
+    for (int y = 0; y < Y_MAX; ++y) {
+      board[x][y] = true;
+    }
+  }
+  ck_assert_int_eq(false, board_at(0, Y_MAX));
 }
 END_TEST
 
-START_TEST(boardSet_toTrue_retrievesTrue)
-{
-    board_init();
-    board_set(50, 50, true);
-    ck_assert_int_eq(true, board_at(50, 50));
+START_TEST(boardSet_toTrue_retrievesTrue) {
+  board_init();
+  board_set(50, 50, true);
+  ck_assert_int_eq(true, board_at(50, 50));
 }
 END_TEST
 
-TCase * tcase_board(void)
-{
-	TCase          *tc;
+TCase *tcase_board(void) {
+  TCase *tc;
 
-	tc = tcase_create("board");
-	tcase_add_test(tc, board_init_setsAllFieldsFalse);
-	tcase_add_test(tc, boardAt_withLocationTrue_returnsTrue);
-	tcase_add_test(tc, boardAt_withLocationFalse_returnsFalse);
-	tcase_add_test(tc, boardAt_wrapsOnXAxis);
-	tcase_add_test(tc, boardAt_withYOverMax_returnsFalse);
-    tcase_add_test(tc, boardSet_toTrue_retrievesTrue);
+  tc = tcase_create("board");
+  tcase_add_test(tc, board_init_setsAllFieldsFalse);
+  tcase_add_test(tc, boardAt_withLocationTrue_returnsTrue);
+  tcase_add_test(tc, boardAt_withLocationFalse_returnsFalse);
+  tcase_add_test(tc, boardAt_wrapsOnXAxis);
+  tcase_add_test(tc, boardAt_withYOverMax_returnsFalse);
+  tcase_add_test(tc, boardSet_toTrue_retrievesTrue);
 
-	return tc;
+  return tc;
 }
 
-Suite          *
-suite_board(void)
-{
-	Suite          *s;
+Suite *suite_board(void) {
+  Suite *s;
 
-	s = suite_create("board");
-	suite_add_tcase(s, tcase_board());
+  s = suite_create("board");
+  suite_add_tcase(s, tcase_board());
 
-	return s;
+  return s;
 }
