2b487e2c0e704e2676c0c3df8f22bd43e9673552
Clay Dowling <clay@lazarusid.com>
2017-04-08 19:25:05 -0400

    Changed return type of board_at to conform to spec

--- board.c
+++ board.c
@@ -6,7 +6,7 @@ bool board[X_MAX][Y_MAX];
 
 void board_init() { memset(board, 0, sizeof(board)); }
 
-bool board_at(int x, int y) {
+int board_at(int x, int y) {
 
   if (x >= X_MAX) {
     x = x % X_MAX;
