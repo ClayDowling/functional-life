bf49ac04d0f8acc1924fdea4f018ff6bd25a206e
Clay Dowling <clay@lazarusid.com>
2017-04-08 12:31:49 -0400

    Drove in test for neighbor board

--- test/suites.h
+++ test/suites.h
@@ -1,7 +1,10 @@
 #ifndef _SUITES_H_
 #define _SUITES_H_
 
+#include <check.h>
+
 Suite *suite_neighbor(void);
 Suite *suite_board(void);
+Suite *suite_neighbor_board(void);
 
 #endif
--- test/testmain.c
+++ test/testmain.c
@@ -1,18 +1,16 @@
-#include <check.h>
 #include "suites.h"
+#include <check.h>
 
-int 
-main(int argc, char **argv)
-{
-	Suite          *s;
-	SRunner        *runner;
-	int		number_fails;
+int main(int argc, char **argv) {
+  SRunner *runner;
+  int number_fails;
 
-	runner = srunner_create(suite_board());
-	srunner_add_suite(runner, suite_neighbor());
-	srunner_run_all(runner, CK_NORMAL);
-	number_fails = srunner_ntests_failed(runner);
-	srunner_free(runner);
+  runner = srunner_create(suite_board());
+  srunner_add_suite(runner, suite_neighbor());
+  srunner_add_suite(runner, suite_neighbor_board());
+  srunner_run_all(runner, CK_NORMAL);
+  number_fails = srunner_ntests_failed(runner);
+  srunner_free(runner);
 
-	return number_fails;
+  return number_fails;
 }
