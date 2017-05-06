3e1d479e6a481814f9ccf7a85859dd2cdc81a49b
Clay Dowling <clay@lazarusid.com>
2017-05-06 09:34:58 -0400

    Fixed funky formatting

--- life_rules.c
+++ life_rules.c
@@ -5,13 +5,13 @@ int life(int x, int y, ptrToGetter getter) {
   int ncount = getter(x, y);
   bool isalive = board_at(x, y);
   if (isalive) {
-  if (2 == ncount || 3 == ncount) {
-    return 1;
-  }
+    if (2 == ncount || 3 == ncount) {
+      return 1;
+    }
   } else {
-          if (3 == ncount) {
-                  return 1;
-          }
+    if (3 == ncount) {
+      return 1;
+    }
   }
   return 0;
 }
